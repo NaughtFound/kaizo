b7bce9f2015979bdebc40de5444a896221fdef9d
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-12-04 20:50:41 +0330

    feat(kaizo): caching added to  ModuleEntry

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+## [0.2.1]
+
+### Added
+
+- caching added to `ModuleEntry`
+
 ## [0.2.0]
 
 ### Added
---
+++ tests/test_num_call.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from kaizo.utils.parser import ConfigParser
+
+RESULT = 1
+
+main_py = """
+num_call = 0
+def fn():
+    global num_call
+
+    num_call+=1
+    return num_call
+
+def fn2(n):
+    return n
+"""
+
+config = """
+local: main.py
+run01:
+  module: local
+  source: fn2
+  call: true
+  args:
+    n:
+        module: local
+        source: fn
+run02:
+    module: local
+    source: fn2
+    args:
+        n: args.n
+"""
+
+
+def test_num_call(tmp_path: Path) -> None:
+    module = tmp_path / "main.py"
+    module.write_text(main_py)
+
+    cfg_file = tmp_path / "cfg.yml"
+    cfg_file.write_text(config)
+
+    parser = ConfigParser(cfg_file)
+    out = parser.parse()
+
+    assert "run02" in out
+    assert out["run02"] == RESULT
