c59750c0c6a0134a444f8c607be8c4c3fc9f4ffe
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-12-11 12:51:32 +0330

    feat(hf): hf plugin completed

--- plugins/kaizo_hf/kaizo/plugins/hf/__init__.py
+++ plugins/kaizo_hf/kaizo/plugins/hf/__init__.py
@@ -1,13 +1,9 @@
-from huggingface_hub import HfApi
+from .common import HFCommit, HFDir, HFPatterns
+from .main import HFPlugin
 
-from kaizo import Plugin
-
-
-class HFPlugin(Plugin):
-    def __init__(self, token: str) -> None:
-        super().__init__()
-
-        self.api = HfApi(token=token)
-
-    def upload_to_hf_hub(self) -> None:
-        pass
+__all__ = (
+    "HFCommit",
+    "HFDir",
+    "HFPatterns",
+    "HFPlugin",
+)
