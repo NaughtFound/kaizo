08f3b3d38a1ceb4e648d894acac4132aa95e6e93
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-12-11 13:25:48 +0330

    feat(hf): creating hf plugin (#12)

--- plugins/kaizo_hf/CHANGELOG.md
+++ plugins/kaizo_hf/CHANGELOG.md
@@ -5,6 +5,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+## [0.1.0]
+
+### Changed
+
+- `HFPlugin` extending `Plugin`
+
 ## [0.0.2]
 
 ### Changed
--- plugins/kaizo_hf/tests/test_import.py
+++ plugins/kaizo_hf/tests/test_import.py
@@ -1,8 +1,7 @@
-import pytest
+import importlib
 
 
 def test_plugin_importable() -> None:
-    try:
-        import kaizo.plugins.hf  # noqa: F401, PLC0415
-    except Exception as e:
-        pytest.fail(f"Importing the plugin raised an exception: {e}")
+    hf = importlib.import_module("kaizo.plugins.hf")
+
+    assert hf
