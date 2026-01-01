5e49f416cb5b96db30526bd66826f653d4d304fb
Mohammad Ghazanfari <mgh.5225@gmail.com>
2026-01-01 15:45:20 +0330

    feat(exception): exception handler added (#26)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+## [1.5.0]
+
+- exception handling added to `ModuleEntry`
+
 ## [1.4.0]
 
 ### Added
--- kaizo/utils/__init__.py
+++ kaizo/utils/__init__.py
@@ -1,6 +1,7 @@
 from .cache import Cacheable
 from .common import extract_variable
 from .entry import DictEntry, Entry, FieldEntry, ListEntry, ModuleEntry
+from .exception import ExceptionHandler, ExceptionPolicy
 from .fn import FnWithKwargs
 from .module import ModuleLoader
 from .storage import Storage
@@ -9,6 +10,8 @@ __all__ = (
     "Cacheable",
     "DictEntry",
     "Entry",
+    "ExceptionHandler",
+    "ExceptionPolicy",
     "FieldEntry",
     "FnWithKwargs",
     "ListEntry",
